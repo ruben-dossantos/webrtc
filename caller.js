a9265fd0e62dea7ab9f0e5a4cf519ba9b99b3753
Ruben Barros <ruben.barros@byside.com>
2018-06-01 12:25:49 +0100

    ssl

--- caller.js
+++ caller.js
@@ -4,7 +4,8 @@ function login(form) {
 	var phone = window.phone = PHONE({
 	    number        : form.username.value || "Anonymous", // listen on username line else Anonymous
 	    publish_key   : 'pub-c-cb1f1183-8885-4291-9a74-60a784b746ac',
-	    subscribe_key : 'sub-c-f4c4555c-658b-11e8-967c-82814fd59ac3',
+        subscribe_key : 'sub-c-f4c4555c-658b-11e8-967c-82814fd59ac3',
+        ssl: true,
 	});	
 	phone.ready(function(){ form.username.style.background="#55ff5b"; });
 	phone.receive(function(session){
